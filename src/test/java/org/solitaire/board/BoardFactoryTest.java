0457b2831b0a7b166e1e23fdd15db157f7bf523c
Tobias <tobias.gaenzler@gmx.de>
2011-09-17 15:32:58 +0200

    First Test for BoardFactory.

---
+++ src/test/java/org/solitaire/board/BoardFactoryTest.java
@@ -0,0 +1,17 @@
+package org.solitaire.board;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * User: Tobias
+ * Date: 17.09.2011
+ */
+public class BoardFactoryTest {
+
+    @Test
+    public void testUnknownBoard() {
+        fail("not implemented yet");
+    }
+}

b733ebe2b67d942c7c03ad16a9c249f6b0733924
unknown <Tobias@.(none)>
2011-10-16 15:02:46 +0200

    Optimized AllSolutionsStrategy: HashSet instead of ArrayList.

--- src/main/java/org/solitaire/solver/strategy/AllSolutionsStrategy.java
+++ src/main/java/org/solitaire/solver/strategy/AllSolutionsStrategy.java
@@ -3,8 +3,10 @@ package org.solitaire.solver.strategy;
 import org.solitaire.board.Board;
 import org.solitaire.board.BoardHelper;
 import org.solitaire.solver.Solution;
+import org.solitaire.util.HashSet;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * This strategy will assemble all reachable positions (modulo symmetry) and calculate all
@@ -14,56 +16,61 @@ import java.util.ArrayList;
  */
 public class AllSolutionsStrategy implements Strategy {
 
-    private ArrayList<ArrayList<Long>>  reachablePositions = new ArrayList<>();
+    private ArrayList<HashSet<Long>> reachablePositions = new ArrayList<>();
 
     @Override
     public Solution solve(Board board, Long startPosition) {
         assembleReachablePositions(board, startPosition);
-        //TODO: remove dead ends, calculate all possible solutions
+        Long start = System.currentTimeMillis();
+        removeDeadEnds(board);
+        System.out.println("Time dead ends: " + (System.currentTimeMillis() - start));
+        //TODO: calculate all possible solutions
         return null;
+    }
+
+    /**
+     * Remove positions which have no consecutive positions from reachable positions (dead ends).
+     * @param board Board the board the positions live on
+     */
+    private void removeDeadEnds(Board board) {
+        for (int pins = 2; pins < reachablePositions.size(); pins++) {
+            Set<Long> positions = reachablePositions.get(pins);
+            ArrayList<Long> deadEnds = new ArrayList<>();
+            for (Long position : positions) {
+                if (board.getConsecutivePositions(position).isEmpty()) {
+                    deadEnds.add(position);
+                }
+            }
+            for (long deadEnd : deadEnds) {
+                positions.remove(deadEnd);
+            }
+        }
     }
 
     /**
      * Assemble all reachable positions starting with startPosition.
      * Store positions in a local field
-     * @param board the board where the positions live
-     * @param startPosition  start finding positions with this start position
+     *
+     * @param board         the board where the positions live
+     * @param startPosition start finding positions with this start position
      */
     private void assembleReachablePositions(Board board, Long startPosition) {
         int numberOfStartPins = BoardHelper.getNumberOfPins(startPosition);
 
-        for(int i=0;i <= numberOfStartPins+1;i++){
-            reachablePositions.add(new ArrayList<Long>());
+        for (int i = 0; i <= numberOfStartPins + 1; i++) {
+            reachablePositions.add(new HashSet<Long>());
         }
 
         // add startPosition to reachablePositions
         reachablePositions.get(numberOfStartPins).add(startPosition);
 
         long totalTime = 0L;
-        for(int numberOfRemainingPieces = numberOfStartPins - 1;
-            // Since it does take too long, we do not run until the end
-            (numberOfRemainingPieces > numberOfStartPins - 8 - 1) && (numberOfRemainingPieces > 0);
-            //(numberOfRemainingPieces > numberOfStartPins - 1) && (numberOfRemainingPieces > 0);
-            numberOfRemainingPieces--) {
+        for (int numberOfRemainingPieces = numberOfStartPins - 1; (numberOfRemainingPieces > 0); numberOfRemainingPieces--) {
             long start = System.currentTimeMillis();
             System.out.println("search positions with " + numberOfRemainingPieces + " of " + numberOfStartPins + " pins");
-            ArrayList<Long> temp = reachablePositions.get(numberOfRemainingPieces + 1);
-            for(Long position : temp) {
-                for(long consecutivePosition: board.getConsecutivePositions(position) ) {
-                    boolean inSet = false;
-                    for(long symPos: board.getSymmetricPositions(consecutivePosition) ) {
-                        //check if a symmetric position is in the set already
-                        if( reachablePositions.get(numberOfRemainingPieces).contains(symPos)) {
-                            inSet = true;
-                            break;
-                        }
-                    }
-                    //add position if no equivalent positions is already in set
-                    if(!inSet) {
-                        reachablePositions.get(numberOfRemainingPieces).add(consecutivePosition);
-                    }
-                }
-            }
+
+            assembleReachablePositions(board, reachablePositions.get(numberOfRemainingPieces + 1),
+                    reachablePositions.get(numberOfRemainingPieces));
 
             totalTime += (System.currentTimeMillis() - start);
             System.out.println("found *" + reachablePositions.get(numberOfRemainingPieces).size() + "* positions in " +
@@ -71,5 +78,33 @@ public class AllSolutionsStrategy implements Strategy {
         }
         System.out.println("TOTAL TIME REACHABLE POSITIONS: " + totalTime + " ms");
 
+    }
+
+    /**
+     * Assemble and store all positions which follow the current positions (modulo symmetry)
+     * @param board the board, the positions live on
+     * @param currentPositions  positions found for a certain number of pins
+     * @param followingPositions  all positions which can follow the current positions (modulo symmetry)
+     */
+    private void assembleReachablePositions(Board board, HashSet<Long> currentPositions, HashSet<Long> followingPositions) {
+        for (Long currentPosition : currentPositions) {
+            for (Long consecutivePosition : board.getConsecutivePositions(currentPosition)) {
+                if(followingPositions.contains(consecutivePosition)) {
+                    continue;
+                }
+                boolean inSet = false;
+                for (Long symPos : board.getSymmetricPositions(consecutivePosition)) {
+                    //check if a symmetric position is already in the set
+                    if (followingPositions.contains(symPos)) {
+                        inSet = true;
+                        break;
+                    }
+                }
+                //add position if no equivalent (symmetric) position is already in set
+                if (!inSet) {
+                    followingPositions.add(consecutivePosition);
+                }
+            }
+        }
     }
 }

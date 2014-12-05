c15d605037fe7f42bc1e870249a1b68ca827b35d
Gustavo <gustavohenrique.86@gmail.com>
2014-12-05 19:09:24 -0300

    Improving ForkJoin usage for better performance
    Using this modification, there is no need to create new List<Long> currentPositions instances during the ForkJoin recursive operation. It will save you some memory, execution time, and energy consumption.

--- src/main/java/org/solitaire/solver/tasks/ParallelStrategyTask.java
+++ src/main/java/org/solitaire/solver/tasks/ParallelStrategyTask.java
@@ -14,6 +14,9 @@ import java.util.concurrent.RecursiveAction;
 public class ParallelStrategyTask extends RecursiveAction {
     private Board board;
     private List<Long> currentPositions;
+    
+    private int from;
+    private int to;
 
     // use a static reference to the resulting positions => all threads can access this set
     public static Set<Long> resultingPositions = new HashSet<>();
@@ -29,14 +32,29 @@ public class ParallelStrategyTask extends RecursiveAction {
         this.board = board;
         this.currentPositions = currentPositions;
     }
+    
+    /**
+     * Constructor which saves the board and the sub-list of current positions.
+     *
+     * @param board            Board where the positions live
+     * @param currentPositions the sub-list of the current positions which are processed by this task
+     * @param from the beginning index of the elements 
+     * @param to the index of the last element
+     */
+    private ParallelStrategyTask(Board board, List<Long> currentPositions, int from, int to) {
+        this(board, currentPositions);
+        this.from = from;
+        this.to = to;
+    }
 
     /**
      * The main work is done here (parallel). Assemble the reachable positions for the sub-list of currentPositions.
      *
      * @param board Board where the positions live
      */
-    private void assembleReachablePositions(Board board) {
-        for (Long currentPosition : currentPositions) {
+    private void assembleReachablePositions(Board board, int from, int to) {
+        for (int i = from; i < to; i++) {
+            Long currentPosition = currentPositions.get(i);
             for (long consecutivePosition : board.getConsecutivePositions(currentPosition)) {
                 // synchronizing on the _contains_ operations is too slow
                 if (ParallelStrategyTask.resultingPositions.contains(consecutivePosition)) {
@@ -64,13 +82,14 @@ public class ParallelStrategyTask extends RecursiveAction {
     @Override
     protected void compute() {
         // avoid concurrency overhead for small sets
-        if (currentPositions.size() < 500000) {
+        //if (currentPositions.size() < 500000) {
+        if (to - from < 500000) {
             assembleReachablePositions(board);
         } else {
             // split the set of current positions in two and start a task for each subset. The result is stored in resultingPositions.
             int split = currentPositions.size() / 2;
-            ParallelStrategyTask left = new ParallelStrategyTask(board, currentPositions.subList(0, split));
-            ParallelStrategyTask right = new ParallelStrategyTask(board, currentPositions.subList(split, currentPositions.size()));
+            ParallelStrategyTask left = new ParallelStrategyTask(board, currentPositions, from, split);
+            ParallelStrategyTask right = new ParallelStrategyTask(board, currentPositions, from + split, to);
             invokeAll(left, right);
         }
     }
